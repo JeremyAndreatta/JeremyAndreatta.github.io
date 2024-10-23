3e2f065dc8d95eb8a6970a95b5454d44645ad57b
JeremyAndreatta <j.andreatta@hotmail.com>
2024-10-23 12:25:36 +1000

    pretty cool title card and background animation

--- portfolio-app/src/App.tsx
+++ portfolio-app/src/App.tsx
@@ -1,35 +1,133 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import './index.css'; // Import Tailwind CSS
+import 'font-awesome/css/font-awesome.min.css';
+
+import { Canvas } from '@react-three/fiber';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const [leftSideButtons, setLeftSideButtons] = useState(true);
+
+  useEffect(() => {
+    const handleMouseMove = (event: MouseEvent) => {
+      const { clientX, clientY } = event;
+      const { innerWidth, innerHeight } = window;
+      const xPercent = (clientX / innerWidth) * 100;
+      const yPercent = (clientY / innerHeight) * 100;
+
+      // Adjust the size of the gradient circle by changing the size value (e.g., 20%).
+      document.body.style.background = `radial-gradient(circle at ${xPercent}% ${yPercent}%, var(--background-secondary-color), var(--background-color))`;
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
+
+  // Add buttons for linkedin github and email in a new tab
+  var externalLinks = [
+    {
+      name: "LinkedIn",
+      link: "https://www.linkedin.com/in/jeremy-andreatta/",
+      icon: "fa fa-linkedin aspect-square"
+    },
+    {
+      name: "GitHub",
+      link: "https://github.com/JeremyAndreatta",
+      icon: "fa fa-github aspect-square "
+    },
+  ]
+
+  //Copy email to clipboard button
+  const copyEmail = () => {
+    navigator.clipboard.writeText("j.andreatta@hotmail.com");
+  }
+
+  const emailButton = {
+    name: "Email",
+    action: copyEmail,
+    icon: "fa fa-envelope aspect-square"
+  }
+
+  const toggleIconsOnLeft = () => {
+    setLeftSideButtons(!leftSideButtons);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="pane" onClick={toggleIconsOnLeft}>
+
+        <div className="flex flex-row">
+
+          {leftSideButtons && (
+            <div className="card flex flex-col justify-center">
+              <div className='pr-10 py-1'>
+                <button className="relative group normal-button flex items-center justify-center text-white text-[25px] w-12 h-12 leading-1" onClick={emailButton.action}>
+                  <i className={emailButton.icon}></i>
+                  <span className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 px-2 py-2 text-xs text-white bg-[var(--accent-color)] rounded opacity-0 group-focus:opacity-100 transition-opacity duration-300">
+                    Email&nbsp;Copied
+                  </span>
+                </button>
+              </div>
+              {externalLinks.map((button, index) => {
+                return (
+                  <a className="pr-10  py-1 flex " href={button.link} target="_blank" key={index} >
+                    <button
+                      className="normal-button flex items-center justify-center text-white text-[25px] w-12 h-12 leading-1"
+                    >
+                      <i className={button.icon}></i>
+                    </button>
+                  </a>
+                );
+              })}
+            </div>
+          )}
+          <div>
+            <div className='flex flex-row flex-wrap -mt-5'>
+              <h1 className="gradient-text">Jeremy&nbsp;</h1>
+              <h1 className="gradient-text">Andreatta</h1>
+            </div>
+
+            <h2 className='text-[var(--secondary-color)]'>mechatronics&nbsp;engineer&nbsp;/ software&nbsp;engineer&nbsp;/ game&nbsp;developer</h2>
+            <h3>i like building things sometimes</h3>
+
+            {!leftSideButtons && (
+
+              <div className="card flex justify-center">
+                <div className='px-5'>
+                  <button className="relative group normal-button flex items-center justify-center text-white text-[25px] w-12 h-12 leading-1" onClick={emailButton.action}>
+                    <i className={emailButton.icon}></i>
+                    <span className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 px-2 py-2 text-xs text-white bg-[var(--accent-color)] rounded opacity-0 group-focus:opacity-100 transition-opacity duration-300">
+                      Email&nbsp;Copied
+                    </span>
+                  </button>
+                </div>
+                {externalLinks.map((button, index) => {
+                  return (
+                    <a className="px-5 flex " href={button.link} target="_blank" key={index} >
+                      <button
+                        className="normal-button flex items-center justify-center text-white text-[25px] w-12 h-12 leading-1"
+                      >
+                        <i className={button.icon}></i>
+                      </button>
+                    </a>
+                  );
+                })}
+              </div>
+            )}
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+
+  );
 }
 
-export default App
+export default App;
